1bdb71c64e89076582243c26485abb85e48f0656
HoucemChhaidar <houcem.chhaidar@gmail.com>
2025-10-13 00:41:54 +0100

    chore: suppress abstract class linter warnings

--- lib/features/auth/domain/interfaces/i_login_repository.dart
+++ lib/features/auth/domain/interfaces/i_login_repository.dart
@@ -1,6 +1,7 @@
 import 'package:credix_app/features/auth/data/models/login_response.dart';
 import 'package:either_dart/either.dart';
 
+// ignore: one_member_abstracts
 abstract class ILoginRepository {
   Future<Either<LoginResponse, String>> login({required String email, required String password});
 }
--- lib/features/wallet/domain/interfaces/i_wallet_repository.dart
+++ lib/features/wallet/domain/interfaces/i_wallet_repository.dart
@@ -1,6 +1,7 @@
 import 'package:credix_app/features/wallet/data/models/wallet_response.dart';
 import 'package:either_dart/either.dart';
 
+// ignore: one_member_abstracts
 abstract class IWalletRepository {
   Future<Either<WalletResponse, String>> myWallet();
 }
